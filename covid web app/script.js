7c1cb1604c8e1a0255ef8b9271c8b7a754bb760f
Mark Kim <koalamarky@outlook.com>
2022-08-19 20:14:01 +1200

    added vanilla js covid tracker app

---
+++ covid web app/script.js
@@ -0,0 +1,31 @@
+
+
+var myForm = document.getElementById('myForm')
+
+myForm = addEventListener('submit', function(e){
+  e.preventDefault()
+
+  var country = this.document.getElementById('country').value
+  
+  var url = "https://api.covid19api.com/total/dayone/country/" +country
+
+   fetch(url)
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res)
+    var length = res.length
+    var index = length - 1 
+    var confirmed = document.getElementById('confirmed')
+    var recovered = document.getElementById('recovered')
+    var deaths = document.getElementById('deaths')
+
+    confirmed.innerHTML = ''
+    recovered.innerHTML = ''
+    deaths.innerHTML = ''
+
+    confirmed.append("Total Confirmed Cases since Feb 28 2020: " + res[index].Confirmed)
+    recovered.append("Total Recovered Cases: " + res[index].Recovered)
+    deaths.append("Total Deaths: " + res[index].Deaths)
+
+  })
+})
